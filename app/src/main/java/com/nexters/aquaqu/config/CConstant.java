dfe5e42954722400170f6fcf1d7fb22bfa25682c
Shin Hyeon Sik <shs9589@gmail.com>
2015-10-24 01:31:25 +0900

    second commit

---
+++ app/src/main/java/com/nexters/aquaqu/config/CConstant.java
@@ -0,0 +1,9 @@
+package com.nexters.aquaqu.config;
+
+/**
+ * Created by HyunSik on 2015-10-23.
+ */
+public class CConstant {
+
+    public static String KEY_CODE ="hMHtzWl7qHvb9kRkjE6XbKxRsSzs05X8nPgokJs3M6bZ%2FUwglNjmQ13dztjJWir0DQotzSi81H1hq2%2FZ1iKxCA%3D%3D";  //키코드
+}
