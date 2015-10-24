7c1770904c1466d3561d660e2930e46e1f286716
DohyunYoun <dhy039@gmail.com>
2015-10-25 01:22:22 +0900

    Pleasdkkdjfkdielsee enter the commit message for your changes. Lines starting

--- app/src/main/java/com/nexters/aquaqu/activity/SettingActivity.java
+++ app/src/main/java/com/nexters/aquaqu/activity/SettingActivity.java
@@ -2,8 +2,6 @@ package com.nexters.aquaqu.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
-import android.view.MenuItem;
 
 import com.nexters.aquaqu.R;
 
@@ -16,24 +14,6 @@ public class SettingActivity extends AppCompatActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.l_activity_setting);
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem menuItem) {
-
-        switch (menuItem.getItemId()) {
-            case android.R.id.home:
-                finish();
-                break;
-            case R.id.action_settings:
-                break;
-        }
-        return (super.onOptionsItemSelected(menuItem));
     }
 }
