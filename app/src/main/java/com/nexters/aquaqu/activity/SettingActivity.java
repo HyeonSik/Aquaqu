a792414b386460203f2cef7dac01445b41b62046
DohyunYoun <dhy039@gmail.com>
2015-10-25 01:52:28 +0900

    setting activity

--- app/src/main/java/com/nexters/aquaqu/activity/SettingActivity.java
+++ app/src/main/java/com/nexters/aquaqu/activity/SettingActivity.java
@@ -2,18 +2,143 @@ package com.nexters.aquaqu.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.nexters.aquaqu.R;
+import com.nexters.aquaqu.util.PreferenceManager;
 
 /**
  * Created by HyunSik on 2015-10-24.
  */
-public class SettingActivity extends AppCompatActivity {
+public class SettingActivity extends AppCompatActivity implements View.OnClickListener {
+    ImageView start00;
+    ImageView start01;
+    ImageView start02;
+    ImageView start03;
+    ImageView start04;
+    ImageView start05;
+    ImageView start06;
+    ImageView start07;
+    ImageView start08;
+    ImageView start09;
+    ImageView start10;
+    TextView setting_name;
+    ImageView setting_exit;
+
+    boolean selected = false;
+
+    int img_selected = 0;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.l_activity_setting);
 
+        start00 = (ImageView) findViewById(R.id.start00);
+        start01 = (ImageView) findViewById(R.id.start01);
+        start02 = (ImageView) findViewById(R.id.start02);
+        start03 = (ImageView) findViewById(R.id.start03);
+        start04 = (ImageView) findViewById(R.id.start04);
+        start05 = (ImageView) findViewById(R.id.start05);
+        start06 = (ImageView) findViewById(R.id.start06);
+        start07 = (ImageView) findViewById(R.id.start07);
+        start08 = (ImageView) findViewById(R.id.start08);
+        start09 = (ImageView) findViewById(R.id.start09);
+        start10 = (ImageView) findViewById(R.id.start10);
+        setting_name = (TextView) findViewById(R.id.setting_name);
+        setting_exit = (ImageView)findViewById(R.id.setting_exit);
+
+
+        start00.setOnClickListener(this);
+        start01.setOnClickListener(this);
+        start02.setOnClickListener(this);
+        start03.setOnClickListener(this);
+        start04.setOnClickListener(this);
+        start05.setOnClickListener(this);
+        start06.setOnClickListener(this);
+        start07.setOnClickListener(this);
+        start08.setOnClickListener(this);
+        start09.setOnClickListener(this);
+        start10.setOnClickListener(this);
+        setting_exit.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        start00.setImageResource(R.drawable.ic_star_outline_white);
+        start01.setImageResource(R.drawable.ic_star_outline_white);
+        start02.setImageResource(R.drawable.ic_star_outline_white);
+        start03.setImageResource(R.drawable.ic_star_outline_white);
+        start04.setImageResource(R.drawable.ic_star_outline_white);
+        start05.setImageResource(R.drawable.ic_star_outline_white);
+        start06.setImageResource(R.drawable.ic_star_outline_white);
+        start07.setImageResource(R.drawable.ic_star_outline_white);
+        start08.setImageResource(R.drawable.ic_star_outline_white);
+        start09.setImageResource(R.drawable.ic_star_outline_white);
+        start10.setImageResource(R.drawable.ic_star_outline_white);
+        switch (v.getId()) {
+            case R.id.start00:
+                start00.setImageResource(R.drawable.ic_star_white);
+                img_selected = 0;
+                setting_name.setText(getResources().getString(R.string.zero));
+                break;
+            case R.id.start01:
+                start01.setImageResource(R.drawable.ic_star_white);
+                img_selected = 1;
+                setting_name.setText(getResources().getString(R.string.one));
+                break;
+            case R.id.start02:
+                start02.setImageResource(R.drawable.ic_star_white);
+                img_selected = 2;
+                setting_name.setText(getResources().getString(R.string.two));
+                break;
+            case R.id.start03:
+                start03.setImageResource(R.drawable.ic_star_white);
+                img_selected = 3;
+                setting_name.setText(getResources().getString(R.string.three));
+                break;
+            case R.id.start04:
+                start04.setImageResource(R.drawable.ic_star_white);
+                img_selected = 4;
+                setting_name.setText(getResources().getString(R.string.four));
+                break;
+            case R.id.start05:
+                start05.setImageResource(R.drawable.ic_star_white);
+                img_selected = 5;
+                setting_name.setText(getResources().getString(R.string.five));
+                break;
+            case R.id.start06:
+                start06.setImageResource(R.drawable.ic_star_white);
+                img_selected = 6;
+                setting_name.setText(getResources().getString(R.string.six));
+                break;
+            case R.id.start07:
+                start07.setImageResource(R.drawable.ic_star_white);
+                img_selected = 7;
+                setting_name.setText(getResources().getString(R.string.seven));
+                break;
+            case R.id.start08:
+                start08.setImageResource(R.drawable.ic_star_white);
+                img_selected = 8;
+                setting_name.setText(getResources().getString(R.string.eight));
+                break;
+            case R.id.start09:
+                start09.setImageResource(R.drawable.ic_star_white);
+                img_selected = 9;
+                setting_name.setText(getResources().getString(R.string.nine));
+                break;
+            case R.id.start10:
+                start10.setImageResource(R.drawable.ic_star_white);
+                img_selected = 10;
+                setting_name.setText(getResources().getString(R.string.ten));
+                break;
+            case R.id.setting_exit:
+                PreferenceManager.getInstance(SettingActivity.this).setSelectedstar(img_selected);
+                finish();
+                break;
+        }
     }
 }
---
+++ app/src/main/java/com/nexters/aquaqu/util/PreferenceManager.java
@@ -0,0 +1,103 @@
+package com.nexters.aquaqu.util;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+/**
+ * This class is for use sharedPreferences to easy on evenywhere.
+ * Implement singleton pattern. If you want add value.
+ * You can modify sample code.
+ * <p>
+ * Sampe code. (You will change all key(KEY,Key,key) to value name use before)
+ * private static final String KEY = "key";
+ * private String key = "";
+ * <p>
+ * public void setKey(String key){
+ * this.key = key;
+ * mEditor.putString(KEY,key);
+ * mEditor.commit();
+ * }
+ * <p>
+ * public String getKey(){
+ * if(key.equals("")){
+ * key = mPrefs.getString("KEY", "");
+ * }
+ * return key;
+ * }
+ * //background select
+ * private static final String BGSELECT = "bgSelect";
+ * private int bgSelect = 1;
+ * <p>
+ * public void setBgSelect(int bgSelect){
+ * this.bgSelect = bgSelect;
+ * mEditor.putInt(BGSELECT, bgSelect);
+ * mEditor.commit();
+ * }
+ * <p>
+ * public int getBgSelect(){
+ * if(bgSelect == 1){
+ * bgSelect = mPrefs.getInt(BGSELECT, 1);
+ * }
+ * return bgSelect;
+ * }
+ */
+
+/*
+*getpref
+* PreferenceManager.getInstance().getPhonenum()
+*
+* setpref
+* PreferenceManager.getInstance().setPphoneNum(phone);
+*
+ */
+
+    /*
+    *
+     */
+
+
+public class PreferenceManager {
+    private static final String PREF_NAME = "aqua_prefs.xml";
+
+    private static PreferenceManager instance;
+
+    public static PreferenceManager getInstance(Context context) {
+        if (instance == null) {
+            instance = new PreferenceManager(context);
+        }
+        return instance;
+    }
+
+    private SharedPreferences mPrefs;
+    private SharedPreferences.Editor mEditor;
+
+    private PreferenceManager(Context context) {
+        mPrefs = context.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE);
+        mEditor = mPrefs.edit();
+    }
+
+    //select star in setting activity
+    private static final String SELECTEDSTAR = "SELECTEDSTAR";
+    private int selected_star = 0;
+    public void setSelectedstar(int key) {
+        this.selected_star = key;
+        clear(SELECTEDSTAR);
+        mEditor.putInt(SELECTEDSTAR, key);
+        mEditor.commit();
+    }
+    public int getSelectedstar() {
+        if (selected_star==0) {
+            selected_star = mPrefs.getInt(SELECTEDSTAR, 0);
+        }
+        return selected_star;
+    }
+
+
+
+    //clear
+    public void clear(String key) {
+        mEditor.remove(key);
+        mEditor.commit();
+    }
+
+}
